a043f51286ab23833c17ff67df94d92eb077b8fa
wilsonlealjunior@hotmail.com <88231845w>
2018-04-11 00:00:50 -0300

    busca gulosa

--- src/principal/BuscaGulosa.java
+++ src/principal/BuscaGulosa.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javax.lang.model.element.QualifiedNameable;
+
 public class BuscaGulosa {
-	
+
 	Node raiz;
 	List<Node> estados;
 
@@ -14,73 +16,77 @@ public class BuscaGulosa {
 		estados = new ArrayList<>();
 		estados.add(raiz);
 	}
-	
+
 	public int realizaBusca() {
 		int quantidadeNosExoandidos = 1;
-		while(!estados.isEmpty()) {
+		while (!estados.isEmpty()) {
 			Node node = buscaHeuristica();
 			estados.remove(node);
 			Principal principal = new Principal(node.n, node.tabuleiro);
-			
-			if(principal.quantidadeRainhas() == node.n) {
-				principal.imprimeTabuleiro();
+
+			if (principal.quantidadeRainhas() == node.n) {
+//				principal.imprimeTabuleiro();
 				return quantidadeNosExoandidos;
 			}
-			
-			if(principal.quantidadeDisponivel() > 0) {
+
+			if (principal.quantidadeDisponivel() > 0) {
 				node.gerarFilhos();
 				estados.addAll(node.filhos);
-				quantidadeNosExoandidos +=  node.filhos.size();
+				quantidadeNosExoandidos += node.filhos.size();
 			}
-			
+
 		}
 		return quantidadeNosExoandidos;
 	}
-	
+
 	private Node buscaHeuristica() {
 		Node retorno = null;
 		int maior = Integer.MIN_VALUE;
-		
+
 		for (Node node : estados) {
 			Principal principal = new Principal(node.n, node.tabuleiro);
-			
-			if(principal.quantidadeRainhas() == node.n) {
+
+			if (principal.quantidadeRainhas() == node.n) {
 				return node;
 			}
-			
-			if(principal.quantidadeDisponivel() == 0) {
+
+			if (principal.quantidadeDisponivel() == 0) {
 				continue;
 			}
-			
-			int valor = principal.quantidadeRainhas()/principal.quantidadeDisponivel();
-			if(valor > maior) {
+
+			int valor = principal.quantidadeDisponivel() + 8 * principal.quantidadeRainhas();
+
+			if (valor > maior) {
 				retorno = node;
-				valor = maior;
+				maior = valor;
 			}
 		}
-		
 		return retorno;
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Guloso");
-		
-		int n = 8;
 
-		Principal principal = new Principal(n);
+		int n = 8;
 
 		Random gerador = new Random();
 
-		principal.tabuleiro[gerador.nextInt(n)][gerador.nextInt(n)] = 1;
+		// principal.tabuleiro[gerador.nextInt(n)][gerador.nextInt(n)] = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.println("Primeira rainha na posicao " + i + " " + j);
+				Principal principal = new Principal(n);
+				principal.tabuleiro[i][j] = 1;
+//				principal.imprimeTabuleiro();
 
-		principal.imprimeTabuleiro();
 
-		System.out.println();
+				Node raiz = new Node(principal.tabuleiro, n);
 
-		Node raiz = new Node(principal.tabuleiro, n);
+				BuscaGulosa busca = new BuscaGulosa(raiz);
 
-		BuscaGulosa busca = new BuscaGulosa(raiz);
+				System.out.println(busca.realizaBusca());
+			}
+		}
 
-		System.out.println(busca.realizaBusca());
 	}
 }
